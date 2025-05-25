21037eadbe1f2c5425e14e9b0ff6013a67ed99a4
ivanreyeschatgpt <ivanreyes.chatgpt@gmail.com>
2025-05-25 10:26:04 -0500

    first commit

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+require('dotenv').config()
 const express = require('express')
+const Note = require('./models/note')
 const app = express()
 
-let notes = [
-  {
-    id: '1',
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: '2',
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: '3',
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-]
+// let notes = [
+//   {
+//     id: '1',
+//     content: 'HTML is easy',
+//     important: true,
+//   },
+//   {
+//     id: '2',
+//     content: 'Browser can execute only JavaScript',
+//     important: false,
+//   },
+//   {
+//     id: '3',
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     important: true,
+//   },
+// ]
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -27,6 +29,35 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const mongoose = require('mongoose')
+
+// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+const password = process.argv[2]
+
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.tvyifu5.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+
+
+const Note = mongoose.model('Note', noteSchema)
+
+
 app.use(express.json())
 app.use(requestLogger)
 app.use(express.static('dist'))
@@ -35,11 +66,19 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
+
+
+/* app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
+ */
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
 
-app.get('/api/notes/:id', (request, response) => {
+/* app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
   const note = notes.find((note) => note.id === id)
 
@@ -48,7 +87,21 @@ app.get('/api/notes/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+}) */
+
+
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
+
+    if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+
+  })
 })
+
 
 const generateId = () => {
   const maxId =
@@ -56,7 +109,7 @@ const generateId = () => {
   return String(maxId + 1)
 }
 
-app.post('/api/notes', (request, response) => {
+/* app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
@@ -75,6 +128,30 @@ app.post('/api/notes', (request, response) => {
 
   response.json(note)
 })
+
+ */
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+
+
+
+
+
 
 app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id
---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+console.log(password)
+
+//const url = `mongodb+srv://fullstack:${password}@cluster0.a5qfl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.tvyifu5.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+/* const note = new Note({
+  content: 'Mongoose makes things easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+}) */
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
