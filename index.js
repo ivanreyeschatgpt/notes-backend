766db88e3206d40c34aecb5f654bb10410565d2b
ivanreyeschatgpt <ivanreyes.chatgpt@gmail.com>
2025-05-25 20:18:17 -0500

    first commit

--- index.js
+++ index.js
@@ -3,23 +3,6 @@ const express = require('express')
 const Note = require('./models/note')
 const app = express()
 
-// let notes = [
-//   {
-//     id: '1',
-//     content: 'HTML is easy',
-//     important: true,
-//   },
-//   {
-//     id: '2',
-//     content: 'Browser can execute only JavaScript',
-//     important: false,
-//   },
-//   {
-//     id: '3',
-//     content: 'GET and POST are the most important methods of HTTP protocol',
-//     important: true,
-//   },
-// ]
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -28,34 +11,6 @@ const requestLogger = (request, response, next) => {
   console.log('---')
   next()
 }
-
-const mongoose = require('mongoose')
-
-// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-const password = process.argv[2]
-
-
-const url = `mongodb+srv://fullstack:${password}@cluster0.tvyifu5.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-
-
-//const Note = mongoose.model('Note', noteSchema)
 
 
 app.use(express.json())
@@ -67,27 +22,11 @@ app.get('/', (request, response) => {
 })
 
 
-
-/* app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
- */
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes)
   })
 })
-
-/* app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find((note) => note.id === id)
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-}) */
 
 
 app.get('/api/notes/:id', (request, response) => {
@@ -102,34 +41,6 @@ app.get('/api/notes/:id', (request, response) => {
   })
 })
 
-
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
-  return String(maxId + 1)
-}
-
-/* app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing',
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
-})
-
- */
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -147,11 +58,6 @@ app.post('/api/notes', (request, response) => {
     response.json(savedNote)
   })
 })
-
-
-
-
-
 
 app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id
@@ -171,11 +77,6 @@ app.put('/api/notes/:id', (request, response) => {
   
   response.json(body)
 })
-
-
-
-
-
 
 
 const unknownEndpoint = (request, response) => {
